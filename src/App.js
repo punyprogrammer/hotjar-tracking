62c948fbd41a73bdf74d89bd7b489841012cfb12
Amardeep Ganguly1 <amardeep.ganguly1@BLN04GW3Q5K7.local>
2022-08-16 16:27:20 +0530

    added-hotjar-inside-useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { hotjar } from 'react-hotjar';
+import {useEffect} from 'react'
 
-hotjar.initialize("3110257" ,'6');
-
-// Identify the user
-hotjar.identify('USER_ID', { userProperty: 'value' });
-
-// Add an event
-hotjar.event('button-click');
+// hotjar.initialize("3110257" ,'6');
+// hotjar.identify('USER_ID', { userProperty: 'value' });
+// hotjar.event('button-click');
 
 function App() {
+  useEffect(()=>{
+   hotjar.initialize(3110257,6)
+  },[])
   return (
        <div>
         tracker
